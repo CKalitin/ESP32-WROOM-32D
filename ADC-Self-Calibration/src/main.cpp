8440ebd89955c5023d886973f75213a2074415a5
Christopher Kalitin <Christopher.Kalitin@gmail.com>
2024-12-26 15:20:18 -0800

    Tested with arduino library
    Using the the arduino library dacWrite() function leads to fluctuation.
    Testing 8 and 250 out of 255 for high and low on the dac pin results in between 0-97 on adc pin for low and 3959-4095 for adc pin for high. Big fluctuations for what should be constant.
    Multimeter reported the same voltage for each high/low. No changes on dac apparently, must be the adc.

--- ADC-Self-Calibration/src/main.cpp
+++ ADC-Self-Calibration/src/main.cpp
@@ -1,15 +1,29 @@
 #include <Arduino.h>
 
+#define DAC_CHANNEL 25
+#define ADC_CHANNEL 36
+
 void setup() {
   Serial.begin(115200);
   Serial.println("Hello World");
-
-  pinMode(14, OUTPUT);
 }
 
 void loop() {
-  digitalWrite(14, HIGH);
-  delay(1000);
-  digitalWrite(14, LOW);
-  delay(1000);
+  dacWrite(DAC_CHANNEL, 8);
+
+  delay(25);
+
+  int adcValue = analogRead(ADC_CHANNEL);
+  Serial.println(adcValue);
+
+  delay(25);
+
+  dacWrite(DAC_CHANNEL, 250);
+
+  delay(25);
+
+  adcValue = analogRead(ADC_CHANNEL);
+  Serial.println(adcValue);
+
+  delay(25);
 }
